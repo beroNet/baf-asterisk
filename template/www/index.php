a69200df05ab40dcd68b97c5ddd2d6d6ff02e02f
Florian Kraatz <fk@beronet.com>
2012-05-22 13:28:13 +0200

    added session management, use userapp-css

--- template/www/index.php
+++ template/www/index.php
@@ -30,7 +30,7 @@ $ast_opts = '-C ' . $conf_path . '/asterisk/asterisk.conf -rnx "sip show peers"'
 
 $sed_exec = 	'sed "s/Dyn Forcerport ACL//" | ' .
 		'sed "s/ D //" | ' .
-		'sed "s/OK (\(.*    \) ms)/OK-(\1_ms)/" | ' .
+		'sed "s/OK (\(.*\) ms)/OK\&nbsp;(\1\&nbsp;ms)/" | ' .
 		'grep -v "Monitored:" | ' .
 		'sed "s/[ ]*/<\/td><td>/g" | '.
 		'sed "s/^<td>//" | ' .
@@ -38,7 +38,7 @@ $sed_exec = 	'sed "s/Dyn Forcerport ACL//" | ' .
 		'sed "s/<td>$//"';
 exec($ast_exec . ' ' . $ast_opts . ' | ' . $sed_exec, $peers_tmp);
 
-$peers_sip = implode('<tr></tr>', $peers_tmp);
+$peers_sip = "<table style=\"width: 100%\">\n<tr>" . implode("</tr>\n<tr>", $peers_tmp) . "</tr>\n</table>\n";
 
 unset($peers_tmp);
 
@@ -56,32 +56,16 @@ echo	"<?xml version=\"1.0\" encoding=\"UTF-8\" ?>\n" .
 	"\t\t\t<div class=\"top\"><img src=\"/app/berogui/includes/images/bg_top.png\"/></div>\n" .
 	"\t\t\t<div class=\"left\">\n" .
 	"\t\t\t\t<h1>" . $app_name . "</h1>\n" .
-	"\t\t\t\t<hr noshade />\n" .
 	"\t\t\t\t<div>\n" .
-	"\t\t\t\t\tMenu: <a href=\"/app/berogui/\">berogui</a> | <a href=\"/userapp/\">UserApp Management</a> | <a href=\"?action=reload\">Reload</a>\n" .
+	"\t\t\t\t\tMenu: \n" .
+	"\t\t\t\t\t\t<a href=\"/app/berogui/\">berogui</a> | \n" .
+	"\t\t\t\t\t\t<a href=\"/userapp/\">UserApp Management</a> | \n" .
+	"\t\t\t\t\t\t<a href=\"filemanager.php\">Asterisk Configuration</a> | \n" .
+	"\t\t\t\t\t\t<a href=\"?action=reload\">Reload</a>\n" .
 	"\t\t\t\t</div>\n" .
-	"\t\t\t\t<h2>Asterisk</h2>\n" .
+	"\t\t\t\t<h2>Status</h2>\n" .
 	"\t\t\t\t<div>\n" .
-	"\t\t\t\t\tYou can use SIP Phones to register at Asterisk with SIP Port 25060.<br />\n" .
-	"\t\t\t\t\tThere are 10 SIP Users: Username=10..20 Secret=10..20<br /><br />\n" .
-	"\t\t\t\t\t<span style=\"font-weight: bold;\">Example SIP Phone Configuration:</span><br /><br />\n" .
-	"\t\t\t\t\tSIP Registrar: "	. $_SERVER['SERVER_NAME']	. ":25060<br />\n" .
-	"\t\t\t\t\tSIP Server: "	. $_SERVER['SERVER_NAME']	. ":25060<br />\n" .
-	"\t\t\t\t\tSIP Proxy: "		. $_SERVER['SERVER_NAME']	. ":25060<br />\n" .
-	"\t\t\t\t\tSIP Server Port: 25060<br />\n" .
-	"\t\t\t\t\tUsername = 10<br />\n" .
-	"\t\t\t\t\tSecret = 10<br /><br />\n" .
-	"\t\t\t\t\t<span style=\"font-weight: bold;\">HowTo Dial</span><br /><br />\n" .
-	"\t\t\t\t\tEvery extensions can reach each other, by just dialing the extensions.\n" .
-	"\t\t\t\t\t<span style=\"font-weight: bold;\">Example:</span>\n" .
-	"\t\t\t\t\tDial 11 to reach Phone 11<br />\n".
-	"\t\t\t\t\tThe beroFix can be reached by prefixing the call with a 0.\n" .
-	"\t\t\t\t\t<span style=\"font-weight: bold;\">Example:</span>\n" .
-	"\t\t\t\t\tDial 01234 to dial 1234 to berofix.<br /><br /><br />\n" .
-	"\t\t\t\t\t<span style=\"font-weight: bold;\">Status:</span><br />\n" .
-	"\t\t\t\t\t<table width=100%>\n" .
 	$peers_sip .
-	"\t\t\t\t\t</table>\n" .
 	"\t\t\t\t</div>\n" .
 	"\t\t\t</div>\n" .
 	"\t\t\t<div class='bottom'><img src=\"/app/berogui/includes/images/bg_bottom.png\"></div>\n" .
