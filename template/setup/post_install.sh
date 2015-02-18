20a89319d4ef073399cb51701835d670846fdc0c
Florian Kraatz <fk@beronet.com>
2015-02-18 18:23:08 +0100

    removed sound-files (they are creeps and don't belong here...)

--- template/setup/post_install.sh
+++ template/setup/post_install.sh
@@ -1,17 +1,9 @@
 #!/bin/bash
 
-
-if [ ! -f /apps/asterisk/etc/asterisk/asterisk.conf ] ; then
-    echo "Copying default confs."
-    mkdir -p /apps/asterisk/etc/asterisk/
-    cp -a /apps/asterisk/conf/* /apps/asterisk/etc/asterisk/
-    sync
-fi
-
-if [ ! -d /apps/asterisk/var/lib/asterisk/moh ] ; then
-    echo "Copying MOH sound files"
-    mkdir -p /apps/asterisk/var/lib/asterisk/moh/
-    cp -a /apps/asterisk/moh/* /apps/asterisk/var/lib/asterisk/moh/
-    sync
+if [ ! -f /apps/asterisk/etc/asterisk/asterisk.conf ]; then
+	echo "Copying default confs."
+	mkdir -p /apps/asterisk/etc/asterisk/
+	cp -a /apps/asterisk/conf/* /apps/asterisk/etc/asterisk/
+	sync
 fi
 
